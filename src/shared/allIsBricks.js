30b547ebf537f290f3bebf46c67326a7d9065d57
HusseinAli97 <venom.blackdesert@gmail.com>
2023-10-14 04:33:57 +0300

    handel ball place

--- src/shared/allIsBricks.js
+++ src/shared/allIsBricks.js
@@ -1,5 +1,5 @@
 import { gameData } from "./gameData";
-export default function AllBroken(bricks, ballObject) {
+export default function AllBroken(bricks, Player, canvas, ballObject) {
 
     let total = 0;
     for (let i = 0; i < bricks.length; i++) {
@@ -12,7 +12,7 @@ export default function AllBroken(bricks, ballObject) {
         gameData.ballObject.speed +=.2;
         gameData.paddle.width -= 15; 
         ballObject.x = gameData.paddle.x + gameData.paddle.width / 2;
-        ballObject.y = gameData.paddle.y - 30;
+        ballObject.y = gameData.paddle.y - 40;
         return
     }
 }
